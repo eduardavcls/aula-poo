5bd00ed34dbd480098db83dd193add04f74920f4
eduarda vasconcelos <eduardavasconcelos145@gmail.com>
2024-06-19 18:52:05 -0300

    lista02-ex2

--- src/main/java/Lista2.java
+++ src/main/java/Lista2.java
@@ -1,8 +1,8 @@
 import java.util.Scanner; 
 
 public class Lista2 {
-    Scanner scanner = new Scanner (System.in);
-    public void ex01(){
+Scanner scanner = new Scanner (System.in);
+ public void ex01(){
     
     int numeroDigitado;
     
@@ -19,5 +19,28 @@ public class Lista2 {
      System.out.println(numeroDigitado + " é 0");
      }
   }
-    
+
+  public void ex02(){
+ 
+    int numeroDigitado1;
+    int numeroDigitado2;
+
+    System.out.println("Escreva dois números e eu direi qual deles é maior.\nInsira o primeiro número:");
+    numeroDigitado1 = scanner.nextInt();
+
+    System.out.println("Agora insira outro número:");
+    numeroDigitado2 = scanner.nextInt();
+
+    if (numeroDigitado1 > numeroDigitado2){
+        System.out.println(numeroDigitado1 + " é maior que " + numeroDigitado2);
+    } else if (numeroDigitado1 == numeroDigitado2){
+        System.out.println(numeroDigitado1 + " é igual a " + numeroDigitado2);
+    } else {
+        System.out.println(numeroDigitado1 + " é menor que " + numeroDigitado2);
+    }
+
+
+
+}
+
 }
